934f3b3c7399768e58822b7e383edae5a1cdffa0
Xasin <logger89@web.de>
2016-05-10 18:53:55 +0200

    Added the basis for a TIMER0 Library.

---
+++ MEGA328/TIMER/Timer0.cpp
@@ -0,0 +1,29 @@
+/*
+ * Timer0.cpp
+ *
+ *  Created on: May 10, 2016
+ *      Author: xasin
+ */
+
+#include "Timer0.h"
+
+namespace Timer0 {
+	void set_prescaler(uint8_t presc) {
+		TCCR0B = presc;
+	}
+	void set_OCR0A(uint8_t value) {
+		OCR0A = value;
+	}
+
+	void set_mode(uint8_t mode) {
+		switch(mode) {
+		case TIMER0_MODE_FREQ:
+			TCCR0A = (1<< WGM01);
+		}
+	}
+
+	void set_OCA0_mode(uint8_t mode) {
+		TCCR0A &= ~(0b1100000);
+		TCCR0A |= mode;
+	}
+}
---
+++ MEGA328/TIMER/Timer0.h
@@ -0,0 +1,36 @@
+/*
+ * Timer0.h
+ *
+ *  Created on: May 10, 2016
+ *      Author: xasin
+ */
+
+#ifndef LIBCODE_TIMER_TIMER0_H_
+#define LIBCODE_TIMER_TIMER0_H_
+
+#include <avr/io.h>
+
+#define TIMER0_PRESC_OFF 	0b000
+#define TIMER0_PRESC_1		0b001
+#define TIMER0_PRESC_8		0b010
+#define TIMER0_PRESC_64		0b011
+#define TIMER0_PRESC_256	0b100
+#define TIMER0_PRESC_1024	0b101
+
+#define TIMER0_MODE_FREQ	1
+
+#define TIMER0_OCA0_OFF		0b00000000
+#define TIMER0_OCA0_TOGGLE	0b01000000
+#define TIMER0_OCA0_CLEAR	0b10000000
+#define TIMER0_OCA0_SET		0b11000000
+
+namespace Timer0 {
+	void set_prescaler(uint8_t presc);
+	void set_OCR0A(uint8_t value);
+
+	void set_mode(uint8_t mode);
+
+	void set_OCA0_mode(uint8_t mode);
+}
+
+#endif /* LIBCODE_TIMER_TIMER0_H_ */
