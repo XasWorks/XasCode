179dd109b16f3e8b07c51e0f8f3dcf6bbe8861c5
Xasin <logger89@web.de>
2016-05-12 21:28:02 +0200

    Renamed the file so that it matches with class name.

---
+++ MEGA328/Communication/IR/IR_LED.cpp
@@ -0,0 +1,16 @@
+/*
+ * IRLED.cpp
+ *
+ *  Created on: 11.05.2016
+ *      Author: xasin
+ */
+
+#include "IR_LED.h"
+
+IR_LED::IR_LED() {
+	// TODO Auto-generated constructor stub
+
+}
+
+void IR_LED::on()  {}
+void IR_LED::off() {}
