55e7896faf61cc949d6c6515a78e49ab7f26e0cf
Xasin <logger89@web.de>
2016-10-25 15:02:23 +0200

    Fertigstellung eines Funktionsfähigen X2 Movable

--- AVR/Movement/X2/X2-Movable.cpp
+++ AVR/Movement/X2/X2-Movable.cpp
@@ -18,20 +18,71 @@ float clamp(float in, float max) {
 namespace X2 {
 
 // Empty constructor
-Movable::Movable() {};
+Movable::Movable(uint16_t updateFrequency) {
+	this->updateFrequency = updateFrequency;
+};
 
 // Set the rotation speed
 void Movable::setRotationSpeed(float speed) {
-	this->rSpeed = clamp(speed, SANE_RSPEED_MAX);
+	this->rSpeed = clamp(speed, SANE_RSPEED_MAX)/updateFrequency;
 }
 // Set the movement speed
 void Movable::setSpeed(float speed) {
-	this->mSpeed = clamp(speed, SANE_MSPEED_MAX);
+	this->mSpeed = clamp(speed, SANE_MSPEED_MAX)/updateFrequency;
 }
 
-// Rotate to a specific angle
-void Movable::rotateTo(float angle) {
+void Movable::rotateBy(float angle) {
+	this->mode = relative;
+	this->rAngle += angle;
+}
 
+void Movable::moveBy(float distance) {
+	this->mode = relative;
+	this->mDistance += distance;
+}
+
+void Movable::continuousMode() {
+	this->mode = continuous;
+	this->mDistance = 0;
+	this->rAngle = 0;
+}
+
+bool Movable::atPosition() {
+	return fabs(this->mDistance) < 0.1;
+}
+bool Movable::atRotation() {
+	return fabs(this->rAngle) < 0.1;
+}
+bool Movable::isReady() {
+	return (this->atPosition() && this->atRotation());
+}
+
+void Movable::flush() {
+	while(!this->isReady()) {
+		_delay_ms(1);
+	}
+}
+
+void Movable::update() {
+	float xThisCal;
+	float rThisCal;
+
+	switch(mode) {
+
+		case relative:
+		xThisCal = clamp(fabs(mSpeed), mDistance);
+		rThisCal = clamp(fabs(rSpeed), rAngle);
+
+		mDistance -= xThisCal;
+		rAngle -= rThisCal;
+
+		Actuator::ISRStepAllBy(xThisCal, rThisCal);
+		break;
+
+		case continuous:
+		Actuator::ISRStepAllBy(mSpeed, rSpeed);
+		break;
+	}
 }
 
 }
--- AVR/Movement/X2/X2-Movable.h
+++ AVR/Movement/X2/X2-Movable.h
@@ -9,61 +9,65 @@
 #define AVR_MOVEMENT_X2_X2_MOVABLE_H_
 
 #include <util/atomic.h>
+#include <util/delay.h>
 #include <math.h>
+
+#include "X2-Actuator.h"
 
 #define SANE_RSPEED_MAX 180
 #define SANE_MSPEED_MAX 200
 
 namespace X2 {
 
+enum MovementMode : uint8_t { continuous, relative };
+
 class Movable {
 protected:
 	// Movement Variables.
-	// These are given in standard units (mm and degrees)
+	// These are given in standard units for absolute positions,
+	// However are given in UNIT/ISR for speed for quicker calculations.
 		// Variables for rotation (speed, angle and est. position)
-		volatile float rAngle = 0, rPosition = 0;
+		volatile float rAngle = 0;
 		float rSpeed = 0;
 
 		// Variables for linear movement (speed and distance)
 		volatile float mDistance = 0;
 		float mSpeed = 0;
 
+		uint16_t updateFrequency;
+
+		MovementMode mode = relative;
+
 public:
 	// Standard constructor. Needs no arguments
-	Movable();
+	Movable(uint16_t updateFrequency);
 
 	// Set rotation speed of the robot in deg/second
 	virtual void setRotationSpeed(float speed);
 	// Set movement speed in mm / second
 	virtual void setSpeed(float speed);
 
-	// Rotate to a specific heading
-	virtual void rotateTo(float angle);
 	// Rotate by a specific amount of degrees
 	virtual void rotateBy(float angle);
-
 	// Move the robot forwards by the specified mm
 	virtual void moveBy(float distance);
 
 	// Set the robot into constant rotation/movement mode
-	// This is done by setting the "leftover movement" variables to infinity,
+	// This is done by setting the "mode" enum to "continuous"
 	// This also makes the system automatically halt once it receives a "normal" movement command
-	virtual void continuousRotation(float speed);
-	virtual void continuousMove(float speed);
+	virtual void continuousMode();
 
 	// Waits for all moves to complete (will not wait for continuous movements to finish!)
 	virtual void flush();
 
 	// Returns true if the robot has finished all non-continuous moves
 	virtual bool isReady();
-	// Returns true if the robot has finished linear movement
+	// Returns true if the robot has finished linear movement (or moves continuously)
 	virtual bool atPosition();
-	// Returns true if the robot has finished rotation movement
+	// Returns true if the robot has finished rotation movement (or moves continuously)
 	virtual bool atRotation();
 
-
-	// Return the current rotation of the robot
-	virtual float getRotation();
+	virtual void update();
 };
 
 }
