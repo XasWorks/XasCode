9eed1c5965ab9b0f7fde1ee74785705bf988f371
David Bailey <davidbailey.2889@gmail.com>
2016-11-04 20:50:15 +0100

    Implementation eines automatischen Battery-Guard

---
+++ AVR/Sensors/Voltage/Battery.cpp
@@ -0,0 +1,42 @@
+/*
+ * Battery.cpp
+ *
+ *  Created on: 04.11.2016
+ *      Author: xasin
+ */
+
+#include "Battery.h"
+#include "../../ADC/ADC_Lib.h"
+
+namespace Voltage {
+
+Battery::Battery(uint8_t pin, float maxMeasured, float min, float max) {
+	this->pin = pin;
+
+	this->convFactor = (maxMeasured/1023);
+
+	this->minVoltage = (uint16_t)(min/maxMeasured * 1023);
+	this->maxVoltage = (uint16_t)(max/maxMeasured * 1023);
+}
+
+float Battery::getVoltage() {
+	return this->measuredVoltage * this->convFactor;
+}
+
+void Battery::setCriticalFunction(void (*onCritical)()) {
+	this->onBatteryCritical = onCritical;
+}
+
+void Battery::update() {
+	ADC_Lib::start_measurement(this->pin);
+}
+void Battery::ADC_update() {
+	if(ADC_Lib::measuredPin == this->pin) {
+		this->measuredVoltage = ADC_Lib::lastResult;
+
+		if(measuredVoltage >= maxVoltage || measuredVoltage <= minVoltage)
+			this->onBatteryCritical();
+	}
+}
+
+} /* namespace Voltage */
