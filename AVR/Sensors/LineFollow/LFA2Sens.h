ec8acf036c474d556be1ec8b4965998418ead0a7
Xasin <logger89@web.de>
2017-02-11 23:20:56 +0100

    Added Intersection-Detecting sensors.

--- AVR/Sensors/LineFollow/LFA2Sens.h
+++ AVR/Sensors/LineFollow/LFA2Sens.h
@@ -7,10 +7,14 @@
 
 #define READING_THRESHOLD 480
 
+#define INTSEC_BUFFER_TICKS 20
+
 namespace LF {
 
 	class ASens2 : public Basic {
 	private:
+		int8_t sigBuffer;
+
 		uint8_t const pin;
 		uint16_t readings[2] = {0, 0};
 
