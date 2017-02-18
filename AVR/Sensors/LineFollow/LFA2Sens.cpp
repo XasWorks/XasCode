8abc11708f8ca891fa562d5ff9185073f357e182
Xasin <logger89@web.de>
2017-02-18 16:12:41 +0100

    Made provisory Intersection detection possible.

--- AVR/Sensors/LineFollow/LFA2Sens.cpp
+++ AVR/Sensors/LineFollow/LFA2Sens.cpp
@@ -4,7 +4,7 @@
 namespace LF {
 
 	ASens2::ASens2(uint8_t const aPin, uint8_t volatile * const ePINx, uint8_t const ePin) :
-		aPin(aPin), ePINx(ePINx), ePin(ePin), sigBuffer(0) {
+		ePINx(ePINx), ePin(ePin), aPin(aPin), sigBuffer(0) {
 	}
 
 	void ASens2::update() {
@@ -13,7 +13,7 @@ namespace LF {
 		updating = 2;
 	}
 
-	void ASens2::ADCUpdate() {
+	void ASens2::ADCReadin() {
 		if(ADC_Lib::measuredPin == aPin) {
 			updating--;
 			readings[0] = ADC_Lib::lastResult;
@@ -22,33 +22,48 @@ namespace LF {
 			updating--;
 			readings[1] = ADC_Lib::lastResult;
 		}
+	}
 
+	void ASens2::NormalLF() {
+		uint16_t totalReading = readings[0] + readings[1];
+
+		if(totalReading >= READING_THRESHOLD) {
+			this->lineStatus = OK;
+			this->lineOffset = (int8_t)((((int32_t)readings[0] - readings[1]) * 127) / totalReading);
+		}
+		else
+			this->lineStatus = LOST;
+	}
+
+	void ASens2::ADCUpdate() {
+		this->ADCReadin();
 
 		if(updating == 0) {
 			uint16_t totalReading = readings[0] + readings[1];
-			this->lineStatus = OK;
+			uint8_t eSensors = (*ePINx >> ePin) & (0b11);
 
-			if(sigBuffer < 0)
-				sigBuffer++;
-			else if(sigBuffer > 0)
-				sigBuffer--;
-
-			if((*ePINx & 1<<ePin) != 0)
-				this->sigBuffer = INTSEC_BUFFER_TICKS;
-			else if((*ePINx & 1 << (ePin+1)) != 0)
-				this->sigBuffer = -INTSEC_BUFFER_TICKS;
-
-			if((*ePINx & (0b11 << ePin)) == (0b11 << ePin))
+			if(eSensors == 0b11) {
 				this->lineStatus = INTSEC;
-			else if(totalReading >= READING_THRESHOLD)
-				this->lineOffset = (int8_t)((((int32_t)readings[0] - readings[1]) * 127) / totalReading);
+				this->lineOffset = 0;
+			}
 			else {
-				this->lineStatus = LOST;
+				this->NormalLF();
 
-				if(sigBuffer < 0)
-					this->lineOffset = -127;
-				else if(sigBuffer > 0)
+				if(this->lineOffset > INTSEC_DISABLE_THRESHOLD)
+					eSensors &= ~(0b01);
+				else if(this->lineOffset < -INTSEC_DISABLE_THRESHOLD)
+					eSensors &= ~(0b10);
+
+				switch(eSensors) {
+				case 0b01:
+					this->lineStatus = INTSEC;
 					this->lineOffset = 127;
+				break;
+				case 0b10:
+					this->lineStatus = INTSEC;
+					this->lineOffset = -127;
+				break;
+				}
 			}
 		}
 	}
--- AVR/Sensors/LineFollow/LFA2Sens.h
+++ AVR/Sensors/LineFollow/LFA2Sens.h
@@ -7,20 +7,23 @@
 
 #define READING_THRESHOLD 480
 
-#define INTSEC_BUFFER_TICKS 20
+#define INTSEC_DISABLE_THRESHOLD 10
 
 namespace LF {
 
 	class ASens2 : public Basic {
 	private:
-		int8_t sigBuffer;
-
 		volatile uint8_t * const ePINx;
 		uint8_t const ePin;
 		uint8_t const aPin;
 		uint16_t readings[2] = {0, 0};
 
+		int8_t sigBuffer = 0;
+
 		volatile uint8_t updating = 0;
+
+		void ADCReadin();
+		void NormalLF();
 
 	public:
 		ASens2(uint8_t const aPin, uint8_t volatile * const ePINx, uint8_t const ePin);
