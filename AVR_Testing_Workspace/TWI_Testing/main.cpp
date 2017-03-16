fc0d1f85d5f69e0b29aeea863ce17b561652cc01
David Bailey <davidbailey.2889@gmail.com>
2017-03-16 23:05:47 +0100

    Added a first very basic testing script!

--- AVR_Testing_Workspace/TWI_Testing/main.cpp
+++ AVR_Testing_Workspace/TWI_Testing/main.cpp
@@ -7,6 +7,8 @@
 
 #include "AVR/Communication/NEW_TWI/TWI.h"
 
+#include <avr/io.h>
+#include <util/delay.h>
 
 uint16_t PINEXPAND = 0;
 
@@ -15,11 +17,26 @@ ISR(TWI_vect) {
 }
 
 void updatePINEX() {
-	while(TWI::readSR() != TWI::Status::IDLE) {};
+	TWI::sendPacketTo(0b01000000, 0x12, &PINEXPAND, 1);
 }
 
 int main() {
 	TWI::init();
+
+	DDRB |= 1<<5;
+
+	PINEXPAND = 0;
+	TWI::sendPacketTo(0b01000000, 0x00, &PINEXPAND, 2);
+
+	while(1) {
+		_delay_ms(1000);
+
+		updatePINEX();
+		PINEXPAND = 1;
+
+		updatePINEX();
+		PINEXPAND = 0;
+	}
 
 	return 0;
 }
