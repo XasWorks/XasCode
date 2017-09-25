5b2e447b8354463eb9fb4a91b8071aa935389742
David Bailey <DavidBailey.2889@gmail.com>
2017-09-25 19:10:28 +0200

    Added brightness profile, adjusted color profile.

--- Ruby/ColorUtils.rb
+++ Ruby/ColorUtils.rb
@@ -36,29 +36,40 @@ class Color
 		self.new([r*brightness, g*brightness, b*brightness]);
 	end
 
-	def self.daylight(brightness = 1, time = nil)
+	def self.daylight(brightness = nil, time: nil)
 		time ||= Time.now();
 
-		m = time.min()/60.0 + time.hour()
+		m = (time.sec()/60 + time.min())/60.0 + time.hour()
 		
 		colorTempPoints = {
-			0  => 2000,
+			0  => 1800,
 			1  => 1400,
 			5  => 1400,
-			6  => 2300,
-			7  => 4500,
-			8  => 5500,
+			7  => 1800,
+			8  => 4000,
 			15 => 5500,
 			16.75 => 3000,
 			17    => 2600,
 			17.25 => 2600,
 			17.5  => 3500,
 			21 => 3000,
-			24 => 2300,
+			23.5  => 2300,
+			24 => 1800,
+		}
+		bGraph = {
+		0   => 0.4,
+		6.9 => 0.4,
+		7   => 0.1,
+		7.5 => 1,
+		21  => 1,
+		23  => 0.7,
+		24  => 0.4,
 		}
 
 		tempGraph = Interpolate::Points.new(colorTempPoints)
 		temperature = tempGraph.at(m);
+		brightGraph = Interpolate::Points.new(bGraph)
+		brightness ||= brightGraph.at(m);
 		self.temperature(temperature, brightness)
 	end
 
