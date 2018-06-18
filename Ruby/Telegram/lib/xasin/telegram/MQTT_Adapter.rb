34ee6f9a4f82883d86f666c6db8cc0a9ecf33407
Xasin <logger89@web.de>
2018-06-18 16:39:08 +0200

    Documentation and code polishing

--- Ruby/Telegram/lib/xasin/telegram/MQTT_Adapter.rb
+++ Ruby/Telegram/lib/xasin/telegram/MQTT_Adapter.rb
@@ -70,93 +70,105 @@ module Telegram
 			def _handle_send(data, uID)
 				# Resolve a saved Username to a User-ID
 				uID = @usernameList[uID] if(@usernameList.key? uID)
-				return if (uID = uID.to_i) == 0;
+				return if (uID = uID.to_i) == 0; # Return if a unknown Username was used
 
-				begin
-					data = JSON.parse(data, symbolize_names: true) if data.is_a? String
-				rescue
-					# Allow for pure-text to be sent (easier on the ESPs)
-					data = {text: data}
-				end
+				gID = data[:gid];
 
-				if(gID = data[:gid])
+				# Check if a GroupID is present and a former message is known
+				if(gID and @groupIDList[uID][gID])
 					if(data[:replace])
 						_handle_delete(gID, uID)
-					elsif(data[:overwrite] and @groupIDList[uID][gID])
+					elsif(data[:overwrite])
 						_handle_edit(data, uID);
-						return;
+						return; # After editing, no new message should be sent!
 					end
 				end
 
+				# Lay out all mandatory parameters for sendMessage request
 				outData = {
 					chat_id: 	uID,
-					parse_mode: (data[:parse_mode] or "Markdown"),
+					parse_mode: (data[:parse_mode] or "Markdown"), # Markdown parse mode is just nice
 					text:			data[:text]
 				}
 
-				if(ilk = data[:inline_keyboard] and data[:gid])
-					outData[:reply_markup] = _process_inline_keyboard(ilk, data[:gid]);
+				# Check if the message is meant to be sent without notification
+				if(data[:silent])
+					outData[:disable_notification] = true;
+				end
+
+				# Check if an inline keyboard layout is given, and parse that.
+				if((ilk = data[:inline_keyboard]))
+					outData[:reply_markup] = _process_inline_keyboard(ilk, gID);
 				end
 
 				reply = @httpCore.perform_post("sendMessage", outData);
-				return unless reply[:ok]
+				return unless reply[:ok]	# Something was wrong about our message layout
+				# TODO Add a proper error handler here.
 
-				# Check if this message has a grouping ID
-				if(gID = data[:gid])
-					# Save this grouping ID
-					@groupIDList[uID][gID] = reply[:result][:message_id];
-				end
+				# If a GroupID was given, save the sent message's ID
+				@groupIDList[uID][gID] = reply[:result][:message_id] if(gID);
 			end
 
 			def _handle_edit(data, uID)
 				# Resolve a saved Username to a User-ID
 				uID = @usernameList[uID] if(@usernameList.key? uID)
-				return if (uID = uID.to_i) == 0;
+				return if (uID = uID.to_i) == 0; # Return if a unknown Username was used
 
-				begin
-					data = JSON.parse(data, symbolize_names: true) if data.is_a? String
+				# Fetch the target MessageID - Return if none is present
+				return unless mID = @groupIDList[uID][data[:gid]]
 
-					# Fetch the target Message ID
-					return unless mID = @groupIDList[uID][data[:gid]]
+				# Lay out all mandatory arguments for the edit POST
+				outData = {
+					chat_id: uID,
+					message_id: mID,
+				};
 
-					outData = {
-						chat_id: uID,
-						message_id: mID,
-					};
+				# If a inline keyboard was given, parse that.
+				if(ilk = data[:inline_keyboard])
+					outData[:reply_markup] = _process_inline_keyboard(ilk, data[:gid]);
+				end
 
-					if(data[:inline_keyboard] and data[:gid])
-						outData[:reply_markup] = _process_inline_keyboard(data[:inline_keyboard], data[:gid]);
-					end
-
-					if(data[:text])
-						outData[:text] = data[:text];
-						# Send the POST request editing the message text
-						@httpCore.perform_post("editMessageText", outData);
-					else
-						@httpCore.perform_post("editMessageReplyMarkup", outData);
-					end
-				rescue
+				if(data[:text]) # Check if text was given
+					outData[:text] = data[:text];
+					# Send the POST request editing the message text
+					@httpCore.perform_post("editMessageText", outData);
+				else
+					# Otherwise, only edit the reply markup (keyboard etc.)
+					@httpCore.perform_post("editMessageReplyMarkup", outData);
 				end
 			end
 
 			def _handle_delete(data, uID)
 				# Resolve a saved Username to a User-ID
 				uID = @usernameList[uID] if(@usernameList.key? uID)
-				return if (uID = uID.to_i) == 0;
+				return if (uID = uID.to_i) == 0; # Return unless the username was known
 
 				# Fetch the real message ID held by a grouping ID
 				return unless mID = @groupIDList[uID][data]
-				@groupIDList[uID].delete(data);
+				@groupIDList[uID].delete(data); # Clear that ID from the list
 
+				# Perform the actual delete
 				@httpCore.perform_post("deleteMessage", {chat_id: uID, message_id: mID});
 			end
 
 			def setup_mqtt()
 				@mqtt.subscribe_to "Telegram/+/Send" do |data, tSplit|
+					begin
+						data = JSON.parse(data);
+					rescue
+						data = {text: data}
+					end
+
 					_handle_send(data, tSplit[0]);
 				end
 
 				@mqtt.subscribe_to "Telegram/+/Edit" do |data, tSplit|
+					begin
+						data = JSON.parse(data);
+					rescue
+						next;
+					end
+
 					_handle_edit(data, tSplit[0])
 				end
 
