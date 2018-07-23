3f47427de71f3c9720ce2a5b99617ee1ad578c43
David Bailey <davidbailey.2889@gmail.com>
2018-07-23 20:39:41 +0200

    Added more documentation, yay!

--- Ruby/MQTT/lib/mqtt/persistence.rb
+++ Ruby/MQTT/lib/mqtt/persistence.rb
@@ -4,6 +4,16 @@ require_relative 'persistence_extensions.rb'
 
 module MQTT
 	class Persistence
+		# Provide persistence for tasks that regularly restart, using MQTT retained messages.
+		# As a fun side effect, this also allows for live monitoring and modifying!
+		#
+		# @author Xasin
+
+
+		# Initialize the Persistence instance.
+		#  This will immediately connect and subscribe to the wildcard of `dir + "/#"`
+		# @param mqtt [MQTT::SubHandler] The MQTT instance to use for storing/communication
+		# @param dir [String] A valid MQTT topic string under which to nest the persistence.
 		def initialize(mqtt, dir = "Persistence")
 			raise ArgumentError, "Not a mqtt class!" unless mqtt.is_a? MQTT::SubHandler
 
@@ -18,6 +28,9 @@ module MQTT
 			end
 		end
 
+
+		# Prepare a piece of data to be sent to the MQTT topic
+		# It must either respond to `to_mqtt_string` or to `to_json`
 		def _prepare_send(data, key)
 			if(data.respond_to? :to_mqtt_string)
 				return data.to_mqtt_string;
@@ -25,7 +38,13 @@ module MQTT
 				return data.to_json
 			end
 		end
+		private :_prepare_send
 
+		# Parse a raw String received from MQTT.
+		# If the given class type responds to `from_mqtt_string`, that
+		# function is used for processing.
+		# `JSON.parse` will be used otherwise.
+		# @return [key-type, nil] Returns the parsed data, nil if that fails.
 		def _parse_received(data, key)
 			begin
 				dType = @paramList[key][:type];
@@ -38,7 +57,10 @@ module MQTT
 				return nil;
 			end
 		end
+		private :_parse_received
 
+		# This processes new data received from MQTT.
+		# It returns if the raw string hasn't changed, and will trigger the callbacks.
 		def _process_data(data, key)
 			return if @rawData[key] == data;
 			@rawData[key] = data;
@@ -56,7 +78,13 @@ module MQTT
 				param[:cbList] = [param[:cbList], param[:change_cb]].flatten;
 			end
 		end
+		private :_process_data
 
+		# Force setting up a key, overwriting what was already configured.
+		# You usually don't need this!
+		# @param key [Symbol] The key that should be set up.
+		# @param type_class [#from_mqtt_string] The class to use to parse the data. A JSON-conversion
+		#  is used if the class doesn't respond to `from_mqtt_string`
 		def setup!(key, type_class = nil)
 			raise ArgumentError, "Key needs to be a symbol!" unless key.is_a? Symbol
 
@@ -74,11 +102,19 @@ module MQTT
 				_process_data(data, key);
 			end
 		end
+		# Set up a key, unless it has already been configured.
+		# @param (See #setup!)
 		def setup(key, type_class = nil)
 			return if @paramList.key? key;
 			setup!(key, type_class);
 		end
 
+		# Read out a given, set-up key, returning `nil` if nothing has been read yet (or the value is nil right now),
+		# returning the data otherwise.
+		# If a block is given (using the on_set alias), this block will be added as a callback.
+		# @param key [Symbol] The key to fetch. Must have been set up first!
+		# @yieldparam newData The newly parsed data (depending on the key's type.)
+		# @yieldparam oldData The former data that was present.
 		def [](key, &callback)
 			raise ArgumentError, "Key needs to be a symbol!" unless key.is_a? Symbol
 			raise ArgumentError, "Key has not been set up!" unless @paramList.key? key;
@@ -92,6 +128,10 @@ module MQTT
 		end
 		alias on_set []
 
+		# Set a given key to a new value, provided the key has been set up.
+		# Will run the `on_change` and `on_set` callbacks immediately, then publish to MQTT.
+		# @param key [Symbol] The key to write. Must have been set up first!
+		# @param data [#to_json, #to_mqtt_string] The data to write.
 		def []=(key, data)
 			raise ArgumentError, "Key needs to be a symbol!" unless key.is_a? Symbol
 			raise ArgumentError, "Key has not been set up!" unless param = @paramList[key];
@@ -114,6 +154,11 @@ module MQTT
 			@mqtt.publish_to "#{@dir}/#{key}", newString, retain: true;
 		end
 
+		# Add a callback to be triggered whenever the key's value changes.
+		# The slight difference to `on_set` is that this callback is not executed on the very first
+		# value received from MQTT, making it easier to not trigger some code on restart, only on actual changes.
+		# @param (See #[])
+		# @yieldparam (See #[])
 		def on_change(key, &callback)
 			raise ArgumentError, "Key needs to be a symbol!" unless key.is_a? Symbol
 			raise ArgumentError, "Key has not been set up!" unless @paramList.key? key;
