b875eca70e6e91710b4bfc2ed42aadd7e47278c0
David Bailey <davidbailey.2889@gmail.com>
2018-07-26 20:44:57 +0200

    TOWARDS OUR GOAL, FELLOW FRIENDS

--- Ruby/GitRestart/lib/git-restart/runner.rb
+++ Ruby/GitRestart/lib/git-restart/runner.rb
@@ -8,25 +8,33 @@ module GitRestart
 
 		attr_accessor :repo, :branches, :exclude_branches, :start_on
 
+		attr_reader	  	:next_tasks
+		attr_reader		:current_task_file
+
 		def current_commit()
 			@git.object("HEAD^").sha;
 		end
 		def current_branch()
 			@git.current_branch();
 		end
+		def current_modified()
+			@current_modified;
+		end
 
 		def initialize()
-			@currentTasks 	= Hash.new();
-			@nextTasks		= Hash.new();
+			GitRestart::Task.runner = self;
+
+			@current_tasks = Hash.new();
+			@next_tasks		= Hash.new();
 
 			@branches 		= Array.new();
 			@exclude_branches = Array.new();
 
 			@branchQueue	= Queue.new();
 
-			yield(self);
-
 			@git = Git.open(".");
+
+			yield(self);
 
 			@mqtt.subscribe_to "GitHub/#{@repo}" do |data|
 				begin
@@ -48,12 +56,62 @@ module GitRestart
 			autostart();
 		end
 
+		def _stop_all_tasks()
+			@current_tasks.each do |name, t|
+				t.stop();
+			end
+			@current_tasks.each do |name, t|
+				t.join();
+			end
+			@current_tasks.clear();
+		end
+
+		def _switch_to(branch, commit = nil)
+			@git.fetch();
+
+			if(branch != current_branch())
+				_stop_all_tasks();
+			end
+			@git.checkout(branch);
+			@git.reset_hard(commit) if commit;
+
+			@git.merge("origin/#{start_on}");
+		end
+
+		def _generate_next_tasks()
+			@next_tasks = Hash.new();
+
+			taskFiles = `find . -iname -nowarn "*.gittask"`
+			taskFiles.split("\n");
+
+			taskFiles.each do |t|
+				t.gsub!(/^\.\//,"");
+				@current_task_file = t;
+
+				# TODO Add proper error reporting
+				begin
+					load(t);
+				rescue LoadError
+					puts("File #{t} could not be loaded!");
+				rescue GitRestart::TaskValidityError
+					puts("Task-File #{t} is not configured properly!");
+				end
+			end
+		end
+
+		def _stop_
+
 		def autostart()
 			return unless @start_on;
 
-			@git.fetch();
-			@git.checkout(@start_on);
-			@git.merge("origin/#{@start_on}");
+			_switch_to(@start_on);
+			_generate_next_tasks();
+
+			@next_tasks.each do |name, t|
+				next unless t.active
+				t.start();
+				@current_tasks[name] = t;
+			end
 		end
 
 		def mqtt=(mqtt)
