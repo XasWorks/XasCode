25624c0afdab65d8d262eead6d365cba7b90aaa0
Xasin <davidbailey.2889@gmail.com>
2019-01-09 21:05:04 +0100

    Began working on a LiPo battery class

---
+++ ESP32/BatteryManager/BatteryManager.cpp
@@ -0,0 +1,17 @@
+/*
+ * BatteryManager.cpp
+ *
+ *  Created on: 9 Jan 2019
+ *      Author: xasin
+ */
+
+#include "BatteryManager.h"
+
+namespace Housekeeping {
+
+BatteryManager::BatteryManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+} /* namespace Housekeeping */
---
+++ ESP32/BatteryManager/include/BatteryManager.h
@@ -0,0 +1,20 @@
+/*
+ * BatteryManager.h
+ *
+ *  Created on: 9 Jan 2019
+ *      Author: xasin
+ */
+
+#ifndef XASLIBS_HOUSEKEEPING_BATTERYMANAGER_BATTERYMANAGER_H_
+#define XASLIBS_HOUSEKEEPING_BATTERYMANAGER_BATTERYMANAGER_H_
+
+namespace Housekeeping {
+
+class BatteryManager {
+public:
+	BatteryManager();
+};
+
+} /* namespace Housekeeping */
+
+#endif /* XASLIBS_HOUSEKEEPING_BATTERYMANAGER_BATTERYMANAGER_H_ */
