4fcb1d6cf89f74b1b5022b411b12a286db8dbbbd
David Bailey <davidbailey.2889@gmail.com>
2019-04-05 09:09:45 +0200

    Added class for the AS1115

---
+++ ESP32/XaI2C/AS1115.cpp
@@ -0,0 +1,55 @@
+/*
+ * AS11115.cpp
+ *
+ *  Created on: 3 Apr 2019
+ *      Author: xasin
+ */
+
+#include "xasin/AS1115.h"
+
+namespace Xasin {
+namespace I2C {
+
+AS1115::AS1115(uint8_t address, i2c_port_t i2c_port)
+	:	segments(),
+		address(address), i2c_port(i2c_port) {
+
+}
+
+void AS1115::send_self_addressing(i2c_port_t i2c_port) {
+	auto i2c_cmd = XaI2C::MasterAction(0);
+
+	uint8_t payload = 1;
+	i2c_cmd.write(0x2D, &payload, 1);
+	i2c_cmd.execute(i2c_port);
+}
+
+void AS1115::send_cmd(uint8_t cmd, uint8_t val) {
+	auto i2c_cmd = XaI2C::MasterAction(address);
+
+	i2c_cmd.write(cmd, &val, 1);
+	i2c_cmd.execute(i2c_port);
+}
+
+void AS1115::init() {
+	send_cmd(0b1100, 1); 	// Start device
+	send_cmd(0x0B, 0x07); 	// Full scan range
+	send_cmd(0x0A, 0x0F);	// Full global brightness
+}
+
+void AS1115::set_segment(uint8_t id, uint8_t code) {
+	if(id > segments.size())
+		return;
+
+	segments[id] = code;
+}
+
+void AS1115::update_segments() {
+	auto i2c_cmd = XaI2C::MasterAction(address);
+
+	i2c_cmd.write(1, segments.data(), 8);
+	i2c_cmd.execute(i2c_port);
+}
+
+} /* namespace I2C */
+} /* namespace Xasin */
---
+++ ESP32/XaI2C/include/xasin/AS1115.h
@@ -0,0 +1,41 @@
+/*
+ * AS11115.h
+ *
+ *  Created on: 3 Apr 2019
+ *      Author: xasin
+ */
+
+#ifndef ESP32_XAI2C_AS1115_H_
+#define ESP32_XAI2C_AS1115_H_
+
+#include <array>
+#include "MasterAction.h"
+
+namespace Xasin {
+namespace I2C {
+
+class AS1115 {
+private:
+	std::array<uint8_t, 8> segments;
+
+	void send_cmd(uint8_t cmd, uint8_t val);
+
+public:
+	const uint8_t address;
+	const i2c_port_t i2c_port;
+
+	static void send_self_addressing(i2c_port_t i2c_port = I2C_NUM_0);
+
+	AS1115(uint8_t address = 0b11, i2c_port_t i2cPort = I2C_NUM_0);
+
+	void init();
+
+	void set_segment(uint8_t id, uint8_t code);
+
+	void update_segments();
+};
+
+} /* namespace I2C */
+} /* namespace Xasin */
+
+#endif /* ESP32_XAI2C_AS1115_H_ */
