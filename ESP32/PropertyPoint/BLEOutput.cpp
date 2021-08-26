1324be9c0d193237fc7b4ea0d170a7b1c27887b4
Xasin <>
2021-08-26 18:34:29 +0200

    Add a BLE Output class for PropertyPoint setting

---
+++ ESP32/PropertyPoint/BLEOutput.cpp
@@ -0,0 +1,34 @@
+
+#include "include/xnm/property_point/BLEOutput.h"
+
+namespace XNM {
+namespace PropertyPoint {
+
+BLEOutput::BLEOutput(Handler &handler, BLE::Server &ble_server) 
+	: 
+	BaseOutput(handler), ble(ble_server, ble_srv_uuid, ble_chr_uuid) {
+
+}
+
+void BLEOutput::send_upd_json(const cJSON * item, BaseProperty &prop) {
+	cJSON * out_obj = cJSON_CreateObject();
+
+	cJSON * upd_obj = cJSON_CreateObject();
+	cJSON_AddItemToObjectCS(out_obj, "upd", upd_obj);
+
+	cJSON_AddItemReferenceToObject(upd_obj, prop.key, const_cast<cJSON*>(item));
+
+	char * out = cJSON_Print(out_obj);
+
+	ble.send_str(out);
+
+	cJSON_free(out);
+	cJSON_Delete(out_obj);
+}
+
+void BLEOutput::init() {
+	ble.init();
+}
+
+}
+}
